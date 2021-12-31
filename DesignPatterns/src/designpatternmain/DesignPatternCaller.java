c19ceedc371a10031ca1385b0a106ab4280ddacb
arthurbz <arthurbziero@gmail.com>
2021-12-31 13:06:41 -0300

    add main to call all design patterns

---
+++ DesignPatterns/src/designpatternmain/DesignPatternCaller.java
@@ -0,0 +1,52 @@
+package designpatternmain;
+
+public class DesignPatternCaller {
+
+	public static void abstractFactory() {
+		System.out.println("Abstract Factory Design Pattern");
+	}
+	
+	public static void bridge() {
+		System.out.println("Bridge Design Pattern");
+	}
+	
+	public static void builder() {
+		System.out.println("Builder Design Pattern");
+	}
+
+	public static void chainOfResponsibility() {
+		System.out.println("Chain of Responsibility Design Pattern");
+	}
+	
+	public static void composite() {
+		System.out.println("Composite Design Pattern");
+	}
+	
+	public static void facade() {
+		System.out.println("Facade Design Pattern");
+	}
+	
+	public static void flyweight() {
+		System.out.println("Flyweight Design Pattern");
+	}
+	
+	public static void mediator() {
+		System.out.println("Mediator Design Pattern");
+	}
+	
+	public static void memento() {
+		System.out.println("Memento Design Pattern");
+	}
+	
+	public static void prototype() {
+		System.out.println("Prototype");
+	}
+	
+	public static void proxy() {
+		System.out.println("Proxy Design Pattern");
+	}
+	
+	public static void visitor() {
+		System.out.println("Visitor Design Pattern");
+	}
+}
---
+++ DesignPatterns/src/designpatternmain/Main.java
@@ -0,0 +1,75 @@
+package designpatternmain;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void printMenu() {
+		System.out.println("\t-----MENU-----");
+		System.out.println("1 - Abstract Factory");
+		System.out.println("2 - Bridge");
+		System.out.println("3 - Builder");
+		System.out.println("4 - Chain of Responsibility");
+		System.out.println("5 - Composite");
+		System.out.println("6 - Facade");
+		System.out.println("7 - Flyweight");
+		System.out.println("8 - Mediator");
+        System.out.println("9 - Memento");
+        System.out.println("10 - Prototype");
+        System.out.println("11 - Proxy");
+        System.out.println("12 - Visitor");
+        System.out.println("Select your option:");
+	}
+	
+	public static String getUserInput() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+	
+	public static void callDesignPatternMain(String option) {
+		switch(option) {
+            case "1":
+            	DesignPatternCaller.abstractFactory();
+                break;
+            case "2":
+            	DesignPatternCaller.bridge();
+                break;
+            case "3":
+            	DesignPatternCaller.builder();
+                break;
+            case "4":
+            	DesignPatternCaller.chainOfResponsibility();
+                break;
+            case "5":
+            	DesignPatternCaller.composite();
+                break;
+            case "6":
+            	DesignPatternCaller.facade();
+                break;
+            case "7":
+            	DesignPatternCaller.flyweight();
+                break;
+            case "8":
+            	DesignPatternCaller.mediator();
+                break;
+            case "9":
+            	DesignPatternCaller.memento();
+                break;
+            case "10":
+            	DesignPatternCaller.prototype();
+                break;
+            case "11":
+            	DesignPatternCaller.proxy();
+                break;
+            case "12":
+            	DesignPatternCaller.visitor();
+                break;
+            default:
+		}
+	}
+	
+	public static void main(String[] args) {
+        printMenu();
+        callDesignPatternMain(getUserInput());
+	}
+}
