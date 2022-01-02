c7bf254b114434069ae720fb0e17cd3feefb336b
arthurbz <arthurbziero@gmail.com>
2022-01-02 18:23:20 -0300

    remove prints and add comments

--- DesignPatterns/src/designpatternmain/DesignPatternCaller.java
+++ DesignPatterns/src/designpatternmain/DesignPatternCaller.java
@@ -41,15 +41,17 @@ public class DesignPatternCaller {
 
 	/* Abstract Factory */
 	public static void abstractFactory() {
-		System.out.println("Abstract Factory Design Pattern\n");
-
+		//Use the AbstractFactory to create a Factory for OffRoad Vehicles
 		AbstractFactory offRoadFactory = FactoryProducer.getFactory("OffRoad");
+		// With the OffRoad Factory created we can produce each OffRoad vehicle
 		Vehicle car1 = offRoadFactory.getVehicle("X");
 		car1.method();
 		Vehicle car2 = offRoadFactory.getVehicle("Y");
 		car2.method();
 
+		//Use the AbstractFactory to create a Factory for Street Vehicles
 		AbstractFactory streetFactory = FactoryProducer.getFactory("Street");
+		// With the Street Factory created we can produce each Street vehicle
 		Vehicle car3 = streetFactory.getVehicle("X");
 		car3.method();
 		Vehicle car4 = streetFactory.getVehicle("Y");
@@ -58,62 +60,64 @@ public class DesignPatternCaller {
 
 	/* Bridge */
 	public static void bridge() {
-		System.out.println("Bridge Design Pattern\n");
-
+		//Create a Truck Vehicle with the Cargo implementation
 		BridgeVehicle truck = new Truck("Scania", new Cargo());
 		truck.work();
 
+		//Create a Bus Vehicle with the People implementation
 		BridgeVehicle bus = new Bus("Mercedes", new People());
 		bus.work();
 	}
 
 	/* Builder */
 	public static void builder() {
-		System.out.println("Builder Design Pattern\n");
-
+		//Create a Director responsible of assembling the car
 		Director director = new Director();
+		//Create a Builder responsible of defining all the steps
 		CarBuilder builder = new CarBuilder();
+		//Build a SportsCar using its constructor
 		director.constructSportsCar(builder);
-
 		Car car = builder.getResult();
 		System.out.println("Car built:\n" + car.getCarType());
-
+		
 		CarManualBuilder manualBuilder = new CarManualBuilder();
-
 		director.constructSportsCar(manualBuilder);
 		Manual carManual = manualBuilder.getResult();
 		System.out.println("\nCar manual built:\n" + carManual.print());
 	}
 
 	public static void chainOfResponsibility() {
-		System.out.println("Chain of Responsibility Design Pattern\n");
-
+		//Creating Handlers of the request
 		LogInfoHandler logInfoHandler = new LogInfoHandler();
 		LogWarningHandler logWarningHandler = new LogWarningHandler();
 		LogErrorHandler logErrorHandler = new LogErrorHandler();
 
 		logInfoHandler.next(logWarningHandler);
 		logWarningHandler.next(logErrorHandler);
-
+		
 		String response = logInfoHandler.handle("error");
 		System.out.println(response);
 	}
 
 	/* Composite */
 	public static void composite() {
-		System.out.println("Composite Design Pattern\n");
-
+		//Item class is the Component, which will be Extended by the Box class
+		//Creates the main box, the top node
 		Box mainBox = new Box(new ArrayList<Item>());
 		mainBox.addItem(new Product("Clean Code: A Handbook of Agile Software Craftsmanship", 200.90));
 
+		//Creating another composite, new box with new children
 		Box smartphoneBox = new Box(new ArrayList<Item>());
+		//Leaf - primitive objects in the composition
 		smartphoneBox.addItem(new Product("Samsung Galaxy S21 Ultra 5G", 3000));
 		smartphoneBox.addItem(new Product("Samsung Galaxy S20 FE", 2000));
 		smartphoneBox.addItem(new Product("iPhone 13", 2000));
 		smartphoneBox.addItem(new Product("iPhone 12", 1500));
 		mainBox.addItem(smartphoneBox);
 
+		//Composite with product children
 		Box laptopBox = new Box(new ArrayList<Item>());
+		//New products = new leafs
 		laptopBox.addItem(new Product("Acer Nitro AN515-44-R5YZ", 5499));
 		laptopBox.addItem(new Product("Dell G15", 5301));
 		mainBox.addItem(laptopBox);
@@ -124,45 +128,50 @@ public class DesignPatternCaller {
 		booksBox.addItem(new Product("Learning Deep Learning - Random Author", 25));
 		mainBox.addItem(booksBox);
 
+		//Calls a composite method
 		System.out.println("Total Price: " + mainBox.calculatePrice());
 	}
 
 	/* Facade */
 	public static void facade() {
-		System.out.println("Facade Design Pattern\n");
-
+		/*
+		 * This Facade runs all the methods of the computer
+		 * While we only call one method, it does all the job for us
+		 * Thats the purpose of the Facade Design Pattern
+		 */
 		FacadeComputer computer = new FacadeComputer();
 		computer.run();
 	}
 
 	/* Flyweight */
 	public static void flyweight() {
-		System.out.println("Flyweight Design Pattern\n");
-
 		Random random = new Random();
+		//Intrinsic Attribute of the objects, it won't ever change
 		String[] playerType = { "Terrorist", "CounterTerrorist" };
+		//Extrinsic Attribute, it may change from object to object
 		String[] weapons = { "AK-47", "AWP", "Knife", "Desert Eagle" };
 
 		for (int i = 0; i < 4; i++) {
+			//In the Factory, it uses the same object to generate new ones
 			Player player = PlayerFactory.getPlayer(playerType[random.nextInt(playerType.length)]);
-
 			player.assignWeapon(weapons[random.nextInt(weapons.length)]);
-
 			player.mission();
 		}
 	}
 
 	/* Mediator */
 	public static void mediator() {
-		System.out.println("Mediator Design Pattern\n");
-
+		//IChatRoom Defines the interface for communication between users
+		//ChatRoom implements the Mediator interface
 		IChatRoom chatroom = new ChatRoom();
 
+		//ChatUser defines the interface for communication with other users
 		User user1 = new ChatUser(chatroom, "1", "Lewis");
 		User user2 = new ChatUser(chatroom, "2", "Max");
 		User user3 = new ChatUser(chatroom, "3", "Lando");
 		User user4 = new ChatUser(chatroom, "4", "Pierre");
 
+		//Add users to the chat
 		chatroom.addUser(user1);
 		chatroom.addUser(user2);
 		chatroom.addUser(user3);
@@ -174,19 +183,19 @@ public class DesignPatternCaller {
 
 	/* Memento */
 	public static void memento() {
-		System.out.println("Memento Design Pattern\n");
-
+		//Object for which the state is to be saved
 		Originator originator = new Originator();
 		History history = new History();
 
+		//Save State #1
 		originator.setState("State #1");
-
+		//Save State #2
 		originator.setState("State #2");
 		history.saveState(originator.generateMemento());
-
+		//Save State #3
 		originator.setState("State #3");
 		history.saveState(originator.generateMemento());
-
+		//Save State #4
 		originator.setState("State #4");
 		System.out.println("Current State: " + originator.getState());
 
@@ -199,7 +208,6 @@ public class DesignPatternCaller {
 
 	/* Prototype */
 	public static void prototype() {
-		System.out.println("Prototype\n");
 		int eid = 123; // Integer.parseInt(br.readLine());
 		System.out.print("\n");
 
@@ -229,8 +237,7 @@ public class DesignPatternCaller {
 
 	/* Proxy */
 	public static void proxy() {
-		System.out.println("Proxy Design Pattern\n");
-
+		//Proxy to represent functionality of another class
 		College college = new CollegeProxy("Wrong");
 		System.out.println("Incorrect Login");
 		System.out.println(college.getCollegeName());
@@ -246,10 +253,10 @@ public class DesignPatternCaller {
 
 	/* Visitor */
 	public static void visitor() {
-		System.out.println("Visitor Design Pattern\n");
-
 		ConcreteElement concreteElement = new ConcreteElement();
+		//Implemented by the visitor and is called for every element in the data structure
 		ConcreteVistor concreteVisitor = new ConcreteVistor();
+		//Methods that accept a visitor
 		concreteElement.accept(concreteVisitor);
 	}
 }
